2f909bf7c43de154a57a02d1f371c64592807bf8
Courtenay <courtenay@entp.com>
2011-04-15 19:21:59 -0700

    Some JSON implementations like to return dates parsed for you into DateTime objects. Quick hack to workaround this (tests broken, pending a refactor)

--- lib/multipass.rb
+++ lib/multipass.rb
@@ -81,8 +81,14 @@ class MultiPass
     options.keys.each do |key|
       options[key.to_sym] = unencode_javascript_unicode_escape(options.delete(key))
     end
+    
+    # Force everything coming out of json into a Time object if it isn't already
+    # with YAJL, it parses dates for us (ugh)
+    if options.has_key?(:expires) && options[:expires].is_a?(String)
+      options[:expires] = Time.parse(options[:expires])
+    end
 
-    if options[:expires].nil? || Time.now.utc > Time.parse(options[:expires])
+    if options[:expires].nil? || Time.now.utc > options[:expires]
       raise MultiPass::ExpiredError.new(data, json, options)
     end
 
